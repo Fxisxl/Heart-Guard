66039bec86f088424d8dc7d76d384311e009b1fe
Faisal <shkfs9058@gmail.com>
2024-10-16 01:56:23 +0530

    edited req.txt

--- api/func.py
+++ api/func.py
@@ -1,9 +1,5 @@
 import asyncio
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 
 import pickle
 with open('api\\heart_disease.pkl', 'rb') as file:
